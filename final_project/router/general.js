9eab10b2e038d45e1e160c4f8aa10835ba197a2a
W18745 <pita2011@live.ca>
2026-03-25 00:19:57 -0230

    New try catch

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,4 +1,5 @@
 const express = require('express');
+//include axios
 const axios = require('axios');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
@@ -39,30 +40,49 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop //Task 10
-public_users.get('/', async function (req, res) {
-  // Send JSON response with formatted friends data
-    try{
-        await axios.json("");
-        res.send(JSON.stringify(books,null,4));
+public_users.get('/', async (req, res) => {
+    try {
+        // Fetch books from your API
+        const response = await axios.get('http://localhost:5000/books');
+
+        // Send the data directly
+        return res.json(response.data);
+
+    } catch (e) {
+        return res.status(400).json({
+            message: "Get books error",
+            error: e.message
+        });
     }
-    catch(e){
-        res.status(400).json({"message":"Get books error"})
-    }
-    
 });
 
 // Get book details based on ISBN //Task 11
-public_users.get('/isbn/:isbn',function (req, res) {
-    isbn = req.params.isbn;
-    return new Promise((resolve, reject) => {
-        if(isbn && books[isbn]){
-            resolve(res.send(JSON.stringify(books[isbn],null,4)));
+
+public_users.get('/isbn/:isbn', async (req, res) => {
+    try {
+        const isbn = req.params.isbn;
+
+        if (!isbn) {
+            return res.status(404).json({ message: "Invalid ISBN" });
         }
-        else{
-            resolve(res.send(`Invalid ISBN`));
-        } 
-        })
- });
+
+        // Fetch all books from API
+        const response = await axios.get('http://localhost:5000/');
+        const books = response.data;
+
+        if (books[isbn]) {
+            return res.json(books[isbn]);
+        } else {
+            return res.status(404).json({ message: "Invalid ISBN" });
+        }
+
+    } catch (error) {
+        return res.status(500).json({
+            message: "Error fetching book",
+            error: error.message
+        });
+    }
+});
   
 // Get book details based on author
 public_users.get('/author/:author', async (req, res) => {
@@ -98,22 +118,34 @@ public_users.get('/author/:author', async (req, res) => {
     }
 });
 
-// Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-    const title = req.params.title;
-    return new Promise((resolve, reject) => {
-        if (title){
-            const bookArray = Object.keys(books).map(key => {
-                  return {[key]:books[key]};
-              });
-            const filterbooks = bookArray.filter(e=>Object.values(e)[0].title === title);
-            resolve(res.send(JSON.stringify(filterbooks,null,4)));
+public_users.get('/title/:title', async (req, res) => {
+    try {
+        const title = req.params.title;
+
+        if (!title) {
+            return res.status(404).json({ message: "Error: Title not Found" });
         }
-        else{
-            resolve(res.status(404).json({message: "Error: Title not Found"});
-        }
-        })
-    
+
+        // Fetch books from API
+        const response = await axios.get('http://localhost:5000/');
+        const books = response.data;
+
+        const bookArray = Object.keys(books).map(key => ({
+            [key]: books[key]
+        }));
+
+        const filterbooks = bookArray.filter(
+            e => Object.values(e)[0].title === title
+        );
+
+        return res.json(filterbooks);
+
+    } catch (error) {
+        return res.status(500).json({
+            message: "Error fetching books",
+            error: error.message
+        });
+    }
 });
 
 //  Get book review
